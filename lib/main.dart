d66aea2f10e0c15a8d00593cf59c0ccc33317adf
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2024-06-03 23:09:59 +0530

    basic.dart main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'basic.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -7,31 +8,3 @@ void main() {
       home: Basic()));
 }
 
-class Basic extends StatelessWidget {
-  const Basic({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.yellow, Colors.greenAccent]),
-        ),
-        alignment: Alignment.center,
-        margin: const EdgeInsets.all(8.0),
-        child: const Text(
-          "Hello world",
-          style: TextStyle(
-            fontSize: 100,
-            color: Colors.red,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'cursive',
-          ),
-        ),
-      ),
-    );
-  }
-}
